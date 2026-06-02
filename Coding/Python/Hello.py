4c4820630e894fbfc023d2c57f0263a72da235d6
Himesh Kandpal <asuskandpal@gmail.com>
2026-06-02 23:44:16 +0530

    Hello.py

--- Coding/Python/Hello.py
+++ Coding/Python/Hello.py
@@ -19,3 +19,24 @@ while count < 10:
     print(count)
     count+=1
 
+# syntax
+
+fruits = ["Apple","Banana","Cherry"]
+for fruit in fruits:
+    print(fruit)
+
+print("\n----------------\n")
+# for loop
+i = 2;
+
+for i in range(2):
+    print("hello kaise ho")
+
+
+print("\n----------------\n")
+# while
+count = 1
+while count < 10:
+    print(count)
+    count+=1
+
