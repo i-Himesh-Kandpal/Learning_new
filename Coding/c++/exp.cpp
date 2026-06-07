758a3c6d9f8a0e279cece9cb6f3a5def52a756f8
Himesh Kandpal <asuskandpal@gmail.com>
2026-06-07 21:23:38 +0530

    exp.cpp

--- Coding/c++/exp.cpp
+++ Coding/c++/exp.cpp
@@ -6,30 +6,26 @@ using namespace std;
 int main(){
 
     int i , j;
-
     int num[5] = {2,3,5,6,8};
     int target = 10;
      
-     
-
-
     for( i = 0; i < 5;i++){
 
         int temp = 0;
 
         for( j = 0; j < 5;j++){
                   
-           temp = 0;
-
+       temp = 0;
        temp = num[i] + num[j]; 
-
-        if(temp == target){
-            cout<<"num["<<i<<"]"<<" + "<<"num ["<<j<<"]"<<" : "<<num[i] + num[j]<<endl;
          
-          }
-         cout<<temp<<endl;
          }
-         
+ cout<<temp<<endl;
+          if(temp == target){
+
+            cout<<"\nnum["<<i<<"]"<<" + "<<"num ["<<j<<"]"<<" : "<<num[i] + num[j]<<endl;
+
+            break;
+          } 
          
     }
   
