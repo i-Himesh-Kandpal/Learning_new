f701312aa106062cd70de288d89c6f9b9b7bd220
Himesh Kandpal <asuskandpal@gmail.com>
2026-06-07 23:35:19 +0530

    exp.cpp

--- Coding/c++/exp.cpp
+++ Coding/c++/exp.cpp
@@ -7,25 +7,38 @@ int main(){
 
     //Two Sum
 
-    vector<int> num = {2,3,5,4,-5,-8,9};
+    vector<int> num = {2,3,5,4};
     int target = 0; 
 
     cout<<"Enter target : ";
     cin>>target;
-    
-    int i , j;
 
-    for( i = 0; i < num.size();i++){
+    for(int i = 0; i < num.size();i++){
 
-        for( j = 0; j < 3;j++){
+      int Target_possibilty = 0;
+      
+        for(int j = i+1; j < num.size();j++){
 
-       if(num[i] + num[j]== target){
-        
+        cout<<"j : "<<j<<" "<<endl;
         cout<<"\n---> "<<"num["<<i<<"]"<<" + "<<"num["<<j<<"] : "<<num[i]+num[j]<<endl;
 
-          }
-       }
+        cout<<"-----"<<endl;
+       if( num[i] + num[j] == target && i != j){
+
+       Target_possibilty++;
+        cout<<"\n---> "<<"num["<<i<<"]"<<" + "<<"num["<<j<<"] : "<<num[i]+num[j]<<endl;
+         
     }
+          cout<<"===="<<endl;
+       
+        }
+       
+        if(Target_possibilty > 0){
+        break;
+       
+    }
+    
+}
   
     return 0;
 }
