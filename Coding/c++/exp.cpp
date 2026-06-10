f45bfd76f86b0e2fe0af181455decb2f6eaef91a
i-Himesh-Kandpal <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>
2026-06-10 17:14:00 +0530

    exe.cpp

--- Coding/c++/exp.cpp
+++ Coding/c++/exp.cpp
@@ -2,30 +2,28 @@
 #include<vector>
 
 using namespace std;
-
+// "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"
 int main(){
 
     //Two Sum
 
     vector<int> num = {2,3,5,4};
-    int target = 0; 
+    int target = 0;
 
     cout<<"Enter target : ";
     cin>>target;
 
     for(int i = 0; i < num.size();i++){
-      
+
         for(int j = i+1; j < num.size();j++){
 
        if( num[i] + num[j] == target && i != j){
 
-        cout<<"\n---> "<<"num["<<i<<"]"<<" + "<<"num["<<j<<"] : "<<num[i]+num[j]<<endl;  
+        cout<<"\n---> "<<"num["<<i<<"]"<<" + "<<"num["<<j<<"] : "<<num[i]+num[j]<<endl;
 
-            }     
+            }
         }
    }
-  
+
     return 0;
 }
-
-    
