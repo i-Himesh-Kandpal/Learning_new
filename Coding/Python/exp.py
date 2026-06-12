9cfd5752caeb04084a513a30e82d728f39288645
i-Himesh-Kandpal <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>
2026-06-12 21:16:04 +0530

    exp.py

--- Coding/Python/exp.py
+++ Coding/Python/exp.py
@@ -2,6 +2,9 @@ a = 2
 b = a + 2
 
 
-print("The value of a :",a)
+print("The value of a :", a)
+print("The value of a :", a)
+print("The value of a :", a)
+print("The value of a :", a)
 print("\n----->after adding a + 2")
-print("\nThe value of b :",b)
+print("\nThe value of b :", b)
